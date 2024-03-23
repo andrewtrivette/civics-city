0ab48705e6914306c5ebcf64d347286f57dd67c2
Andrew Trivette <andrew.trivette@gmail.com>
2024-03-23 15:55:32 -0400

    Delete site/deploy.js

--- site/deploy.js
+++
@@ -1,5 +0,0 @@
-import {execSync} from 'child_process';
-
-execSync('aws s3 sync ./atlanta s3://atlanta.civics.city --delete --acl public-read');
-execSync('aws cloudfront create-invalidation --distribution-id E1L64O4L39YXBP --paths "/*"');
-console.log('completed!');
